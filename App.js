2b9f819395f02b05f35a903662b609b610447395
saalim95 <saalim@paralleldots.com>
2023-03-14 12:08:25 +0530

    added all listeners

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Button, NativeModules, NativeEventEmitter} from 'react-native';
+import { onCaptureHandler, onDismissPressHandler, onDirectionPressHandler, onDiscardImageHandler, onSubmitBarcodeImageHandler } from './sdkCam';
 class App extends React.Component {
   componentDidMount() {
     this._addListenersForNativeCamSDK();
@@ -11,13 +12,52 @@ class App extends React.Component {
     this.eventCaptureListener?.remove();
   }
 
-  _addListenersForNativeCamSDK = () => {
-    console.log(13);
-    const eventEmitter = new NativeEventEmitter();
+   _addListenersForNativeCamSDK = () => {
+    const eventEmitter = new NativeEventEmitter(NativeModules.Bridge);
     this.eventCaptureListener = eventEmitter.addListener(
-      "JS-Event",
-      nativeData => {
-        console.log({nativeData});
+      'CaptureEvent',
+      ({state, image_base64}) => {
+        console.log('683');
+        onCaptureHandler(state, image_base64);
+      },
+    );
+
+    this.eventDirectionListener = eventEmitter.addListener(
+      'DirectionEvent',
+      ({state, direction}) => {
+        onDirectionPressHandler(state, direction);
+      },
+    );
+
+    this.eventDiscardListener = eventEmitter.addListener(
+      'DiscardCaptureEvent',
+      ({state}) => {
+        console.log(
+          '686: data received on discard',
+          JSON.stringify(state, null, 1),
+        );
+        onDiscardImageHandler(state);
+      },
+    );
+
+    this.eventSubmitListener = eventEmitter.addListener(
+      'SubmitImageEvent',
+      ({state, results, mode, upload_params}) => {
+        return
+        // upload_params = JSON.parse(upload_params);
+        // console.log(
+        //   '711: data received on submit',
+        //   JSON.stringify({state, mode, upload_params}, null, 1),
+        // );
+        // const callBack = this._navigateToReviewCallbackForDL;
+        // onSubmitBarcodeImageHandler(state, mode, upload_params, callBack);
+      },
+    );
+
+    this.eventDismissCameraListener = eventEmitter.addListener(
+      'DismissCameraEvent',
+      ({state}) => {
+        onDismissPressHandler(state.previewList.length);
       },
     );
   };
